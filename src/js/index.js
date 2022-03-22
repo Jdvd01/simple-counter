b88c8da047eb9514f76c19998ab24d03974d3c80
Jose Velasquez <velasquezdenorajose@gmail.com>
2022-03-22 17:46:26 -0400

    Entrega contador

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,40 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import propTypes from "prop-types";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+const Contador = (props) => {
+	return (
+		<div className="contador">
+			<div className="reloj">
+				<i className="fas fa-clock"></i>
+			</div>
+			<div className="tercero">{props.digitoTres % 10}</div>
+			<div className="segundo">{props.digitoDos % 10}</div>
+			<div className="primero">{props.digitoUno % 10}</div>
+		</div>
+	);
+};
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+Contador.propTypes = {
+	digitoUno: propTypes.number,
+	digitoDos: propTypes.number,
+	digitoTres: propTypes.number,
+};
+
+let counter = 0;
+setInterval(() => {
+	let tres = Math.floor(counter / 100);
+	let dos = Math.floor(counter / 10);
+	let uno = Math.floor(counter / 1);
+	counter++;
+
+	//render your react application
+	ReactDOM.render(
+		<Contador digitoUno={uno} digitoDos={dos} digitoTres={tres} />,
+		document.querySelector("#app")
+	);
+}, 1000);
